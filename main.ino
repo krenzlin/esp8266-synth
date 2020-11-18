60fbd1b18395bcfe393cf8ba74e52cad44bfb767
Konrad Krenzlin <konrad.krenzlin@hfbk-hamburg.de>
2020-11-18 15:36:51 +0100

    Add blink example and fix Makefile

--- main.ino
+++ main.ino
@@ -1,5 +1,11 @@
-#include "Arduino.h"
 
-void setup() {};
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
 
-void loop() {};
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
