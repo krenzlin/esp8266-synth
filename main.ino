70ede463526ab18d9a00fb158f35864cc5317a9a
Konrad Krenzlin <konrad.krenzlin@hfbk-hamburg.de>
2020-11-19 19:00:47 +0100

    Do not set pinmode (LED is same as I2S word select)

--- main.ino
+++ main.ino
@@ -1,17 +1,20 @@
 #include "Arduino.h"
+#include <user_interface.h>
 
 #include "hal.h"
+#include "osc.h"
+#include "misc.h"
 
+
+auto synth = OSC::Saw();
 
 void setup() {
+    system_update_cpu_freq(160);
     HAL::WIFI::turn_off();
     HAL::I2S::init(44100);
-    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);                       // wait for a second
+    static auto sample = synth.sample();
+    HAL::I2S::write(sample);
 }
