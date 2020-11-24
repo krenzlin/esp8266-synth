ada1241086e6bd2d12f99f58ea3395ae733dee2c
Konrad Krenzlin <konrad.krenzlin@hfbk-hamburg.de>
2020-11-24 18:04:51 +0100

    Example sketch w/ speeding up bass drum using clock

--- main.ino
+++ main.ino
@@ -1,23 +1,38 @@
 #include "hal.h"
+#include "clock.h"
 #include "osc.h"
 #include "misc.h"
-
 
 auto synth1 = OSC::Saw();
 auto synth2 = OSC::Saw();
 auto drum = OSC::Sampler();
+auto clk = Clock(44100, 1);
+uint16_t bpm {120};
+
 
 void setup() {
     HAL::set_cpu_freq(160);
     HAL::WIFI::turn_off();
     HAL::I2S::init(44100);
 
-    synth1.set_note(74);
-    synth2.set_note(69);
+    auto cb = [&]() mutable {drum.on();};
+    clk.set_pulse_callback(cb);
+    clk.start(bpm);
 }
 
+
 void loop() {
-    auto sample = (synth1.sample() + synth2.sample()) >> 8;
-    sample += drum.sample() >> 6;
+    static uint32_t ticks {0};
+    ticks++;
+    if (ticks > 44100) {
+        bpm += 5;
+        if (bpm > 200) {
+            bpm = 60;
+        }
+        clk.set_bpm(bpm);
+        ticks = 0;
+    }
+    clk.tick();
+    uint16_t sample = drum.sample() >> 6;
     HAL::I2S::write(sample, sample);
 }
