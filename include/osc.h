a651481fd9875f967e461dab1949b95b485a9725
Konrad Krenzlin <konrad.krenzlin@hfbk-hamburg.de>
2020-12-04 12:00:40 +0100

    Use google style naming in osc

--- include/osc.h
+++ include/osc.h
@@ -5,8 +5,8 @@ namespace osc {
     const uint16_t ZERO = 0xFFFF >> 1;
 
     class Saw {
-        uint16_t phase {0};
-        uint16_t p_incr {0};
+        uint16_t phase_ {0};
+        uint16_t p_incr_ {0};
 
         public:
             uint16_t sample();
@@ -16,19 +16,19 @@ namespace osc {
     class Sampler {
         const uint16_t *sample_;
         uint32_t len_ {0};
-        uint32_t index {0};
+        uint32_t index_ {0};
         uint32_t end_ {0};
-        float max_vol {1.0f};
-        float vol {1.0f};
+        float max_vol_{1.0f};
+        float vol_ {1.0f};
 
         public:
             Sampler(const uint16_t sample[], const uint32_t len, const float vol=1.f) :
                 sample_{sample},
                 len_{len},
-                index{len},
+                index_{len},
                 end_{len},
-                max_vol{vol},
-                vol{vol}
+                max_vol_{vol},
+                vol_{vol}
                 {};
             uint16_t sample();
             void on(const uint32_t note);
--- src/osc.cpp
+++ src/osc.cpp
@@ -7,37 +7,37 @@
 #endif
 
 void osc::Saw::on(const uint32_t note) {
-    p_incr = lut::mtoincr[note];
+    p_incr_ = lut::mtoincr[note];
 }
 
 uint16_t osc::Saw::sample() {
-    uint16_t sample = phase;
+    uint16_t sample = phase_;
 
-    phase += p_incr;
-    if (phase > 0xFFFF) {  // compiler will optimize this away as int16 wraps around 0xFFFF
-        phase -= 0xFFFF;
+    phase_ += p_incr_;
+    if (phase_ > 0xFFFF) {  // compiler will optimize this away as int16 wraps around 0xFFFF
+        phase_ -= 0xFFFF;
     }
 
     return sample;
 }
 
 void osc::Sampler::on(const uint32_t note) {
-    index = 0;
+    index_ = 0;
     end_ = len_; // * misc::fast_float_rand();
-    vol = max_vol; // * misc::fast_float_rand();
+    vol_ = max_vol_; // * misc::fast_float_rand();
 }
 
 uint16_t osc::Sampler::sample() {
     uint16_t sample {osc::ZERO};
 
-    if (index < end_) {
+    if (index_ < end_) {
 #if ARDUINO
         sample = pgm_read_word_near(sample_ + index);
 #else
-        sample = sample_[index];
+        sample = sample_[index_];
 #endif
-        index++;
+        index_++;
     }
 
-    return dsp::volume(sample, vol);
+    return dsp::volume(sample, vol_);
 }
